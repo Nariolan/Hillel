3b3eb83f15615ab366cb012f7319fd2c781bd0f1
Nariolan <65863965+Nariolan@users.noreply.github.com>
2021-03-28 00:18:38 +0200

    Update Homework№13.py

--- Homework№13.py
+++ Homework№13.py
@@ -3,9 +3,11 @@ import random, string
 
 class EmailGenerator:
 
-    def __init__(self, list_1, list_2, min_limit=100, max_limit=999):
-        self.min_limit = min_limit
-        self.max_limit = max_limit
+    def __init__(self, list_1, list_2, min_str_limit=5, max_str_limit=7, min_int=100, max_int=999):
+        self.min_limit = min_str_limit
+        self.max_limit = max_str_limit
+        self.min_int = min_int
+        self.max_int = max_int
         self.list_1 = list_1
         self.list_2 = list_2
 
@@ -16,7 +18,7 @@ class EmailGenerator:
 
     def generate_email(self):
         name_part = random.choice(self.list_1)
-        int_part = str(random.randint(100, 999))
+        int_part = str(random.randint(self.min_int, self.max_int))
         string_part = self.generate_str()
         domain_part = random.choice(self.list_2)
         email = f"{name_part}.{int_part}@{string_part}.{domain_part}"
@@ -25,6 +27,6 @@ class EmailGenerator:
 
 names = ["king", "miller", "kean"]
 domains = ["net", "com", "ua"]
-generator_1 = EmailGenerator(names, domains, 5, 7)
+generator_1 = EmailGenerator(names, domains)
 print(generator_1.generate_str())
 print(generator_1.generate_email())
